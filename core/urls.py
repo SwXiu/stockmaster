476bf9f76dd545033acbff6f18bbfff6cad2e08f
SwXiu <sw.xiu01@gmail.com>
2025-01-29 12:57:33 +0100

    interfaz registro

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import HomePage
+from .views import HomePage, AdminInterface, ClientInterface
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomePage.as_view(), name="home"),
+    path('user/', include('users.urls', namespace='user')),  
+    path('clientV/', ClientInterface.as_view(), name="clientInterface"),
+    path('adminV/', AdminInterface.as_view(), name="adminInterface"),
 ]
